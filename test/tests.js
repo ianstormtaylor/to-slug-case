314ade1dbddd7f94fbb50f9f29521ef49bc83adc
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-18 22:40:20 -0700

    fix

--- test/tests.js
+++ test/tests.js
@@ -1,30 +1,34 @@
-describe('to-snake-case', function () {
+describe('to-slug-case', function () {
 
 var assert = require('assert');
-var snake = require('to-snake-case');
+var slug = require('to-slug-case');
 
-it('space case', function () {
-  assert('space_case' == snake('space case'));
+it('shouldnt touch slug case', function () {
+  assert('a-slug-case-string' == slug('a-slug-case-string'));
 });
 
-it('camelCase', function () {
-  assert('camel_case' == snake('camelCase'));
+it('should convert camel case', function () {
+  assert('a-camel-case-string' == slug('aCamelCaseString'));
 });
 
-it('snake_case', function () {
-  assert('snake_case' == snake('snake_case'));
+it('should convert snake case', function () {
+  assert('a-snake-case-string' == slug('a_snake_case_string'));
 });
 
-it('dot.case', function () {
-  assert('dot_case' == snake('dot.case'));
+it('should convert space case', function () {
+  assert('a-space-case-string' == slug('a space case string'));
 });
 
-it('weird[case', function () {
-  assert('weird_case' == snake('weird[case'));
+it('should convert dot case', function () {
+  assert('a-dot-case-string' == slug('a.dot.case.string'));
 });
 
-it('complexCamelCase', function () {
-  assert('complex_camel_case' == snake('complexCamelCase'));
+it('should convert title case', function () {
+  assert('a-title-case-of-string' == slug('A Title: Case of String'));
+});
+
+it('should convert constant case', function () {
+  assert('a-constant-case-string' == slug('A_CONSTANT_CASE_STRING'));
 });
 
 });
